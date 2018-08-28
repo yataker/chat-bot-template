2155932f8ff5f1f169fa11fdf4089070d92ddf70
yataker <42265083+yataker@users.noreply.github.com>
2018-08-28 12:50:22 +0900

    オウム返し

--- index.php
+++ index.php
@@ -11,134 +11,33 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
 // 署名が正当かチェック。正当であればリクエストをパースし配列へ
-$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+// 不正であれば例外の内容を出力
+try {
+  $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+  error_log('parseEventRequest failed. InvalidSignatureException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
+  error_log('parseEventRequest failed. UnknownEventTypeException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
+  error_log('parseEventRequest failed. UnknownMessageTypeException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+  error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
+}
+
 // 配列に格納された各イベントをループで処理
 foreach ($events as $event) {
-  // イベントがPostbackEventクラスのインスタンスであれば
-  if ($event instanceof \LINE\LINEBot\Event\PostbackEvent) {
-    // テキストを返信し次のイベントの処理へ
-    replyTextMessage($bot, $event->getReplyToken(), 'Postback受信「' . $event->getPostbackData() . '」');
+  // MessageEventクラスのインスタンスでなければ処理をスキップ
+  if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
+    error_log('Non message event has come');
     continue;
   }
-  // テキストを返信
-  //$bot->replyText($event->getReplyToken(), 'TextMessage');
-  // テキストを返信
-  //replyTextMessage($bot, $event->getReplyToken(), 'TextMessage');
-  // 画像を返信
-  //replyImageMessage($bot, $event->getReplyToken(), 'https://' . $_SERVER['HTTP_HOST'] . '/imgs/original.jpg', 'https://' . $_SERVER['HTTP_HOST'] . '/imgs/preview.jpg');
-  // 位置情報を返信
-  //replyLocationMessage($bot, $event->getReplyToken(), 'LINE', '東京都渋谷区渋谷2-21-1 ヒカリエ27階', 35.659025, 139.703473);
-  // スタンプを返信
-  //replyStickerMessage($bot, $event->getReplyToken(), 1, 1);
-  // 動画を返信
-  //replyVideoMessage($bot, $event->getReplyToken(),
-  //  'https://' . $_SERVER['HTTP_HOST'] . '/videos/sample.mp4',
-  //  'https://' . $_SERVER['HTTP_HOST'] . '/videos/sample_preview.jpg');
-  // オーディファイルを返信
-  //replyAudioMessage($bot, $event->getReplyToken(), 'https://' . $_SERVER['HTTP_HOST'] . '/audios/sample.m4a', 6000);
-  /*
-  // 複数のメッセージをまとめて返信
-  replyMultiMessage($bot, $event->getReplyToken(),
-    new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('TextMessage'),
-    new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder('https://' . $_SERVER['HTTP_HOST'] . '/imgs/original.jpg', 'https://' . $_SERVER['HTTP_HOST'] . '/imgs/preview.jpg'),
-    new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder('LINE', '東京都渋谷区渋谷2-21-1 ヒカリエ27階', 35.659025, 139.703473),
-    new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder(1, 1)
-  );
-  */
-  /*
-  // Buttonsテンプレートメッセージを返信
-  replyButtonsTemplate($bot,
-    $event->getReplyToken(),
-    'お天気お知らせ - 今日は天気予報は晴れです',
-    'https://' . $_SERVER['HTTP_HOST'] . '/imgs/template.jpg',
-    'お天気お知らせ',
-    '今日は天気予報は晴れです',
-    // タップ時、テキストをユーザーに発言させるアクション
-    new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder (
-      '明日の天気', 'tomorrow'),
-    // タップ時、テキストをBotに送信するアクション(トークには表示されない)
-    new LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder (
-      '週末の天気', 'weekend'),
-    // タップ時、URLを開くアクション
-    new LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder (
-      'Webで見る', 'http://google.jp')
-  );
-  */
-  /*
-  // Confirmテンプレートメッセージを返信
-  replyConfirmTemplate($bot,
-    $event->getReplyToken(),
-    'Webで詳しく見ますか？',
-    'Webで詳しく見ますか？',
-    new LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder (
-      '見る', 'http://google.jp'),
-    new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder (
-      '見ない', 'ignore')
-  );
-  */
-  /*
-  // Carouselテンプレートメッセージを返信
-  // ダイアログの配列
-  $columnArray = array();
-  for($i = 0; $i < 5; $i++) {
-    // アクションの配列
-    $actionArray = array();
-    array_push($actionArray, new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder (
-      'ボタン' . $i . '-' . 1, 'c-' . $i . '-' . 1));
-    array_push($actionArray, new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder (
-      'ボタン' . $i . '-' . 2, 'c-' . $i . '-' . 2));
-    array_push($actionArray, new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder (
-      'ボタン' . $i . '-' . 3, 'c-' . $i . '-' . 3));
-    // CarouselColumnTemplateBuilderの引数はタイトル、本文、
-    // 画像URL、アクションの配列
-    $column = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder (
-      ($i + 1) . '日後の天気',
-      '晴れ',
-      'https://' . $_SERVER['HTTP_HOST'] .  '/imgs/template.jpg',
-      $actionArray
-    );
-    // 配列に追加
-    array_push($columnArray, $column);
+  // TextMessageクラスのインスタンスでなければ処理をスキップ
+  if (!($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
+    error_log('Non text message has come');
+    continue;
   }
-  replyCarouselTemplate($bot, $event->getReplyToken(),'今後の天気予報', $columnArray);
-  */
-  /*
-  // ユーザーから送信された画像ファイルを取得し、サーバーに保存する
-  // イベントがImageMessage型であれば
-  if ($event instanceof \LINE\LINEBot\Event\MessageEvent\ImageMessage) {
-    // イベントのコンテンツを取得
-    $content = $bot->getMessageContent($event->getMessageId());
-    // コンテンツヘッダーを取得
-    $headers = $content->getHeaders();
-    // 画像の保存先フォルダ
-    $directory_path = 'tmp';
-    // 保存するファイル名
-    $filename = uniqid();
-    // コンテンツの種類を取得
-    $extension = explode('/', $headers['Content-Type'])[1];
-    // 保存先フォルダが存在しなければ
-    if(!file_exists($directory_path)) {
-      // フォルダを作成
-      if(mkdir($directory_path, 0777, true)) {
-        // 権限を変更
-        chmod($directory_path, 0777);
-      }
-    }
-    // 保存先フォルダにコンテンツを保存
-    file_put_contents($directory_path . '/' . $filename . '.' . $extension, $content->getRawBody());
-    // 保存したファイルのURLを返信
-    replyTextMessage($bot, $event->getReplyToken(), 'http://' . $_SERVER['HTTP_HOST'] . '/' . $directory_path. '/' . $filename . '.' . $extension);
-  }
-  */
-  // ユーザーのプロフィールを取得しメッセージを作成後返信
-  $profile = $bot->getProfile($event->getUserId())->getJSONDecodedBody();
-  $bot->replyMessage($event->getReplyToken(),
-    (new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder())
-      ->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('現在のプロフィールです。'))
-      ->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('表示名：' . $profile['displayName']))
-      ->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('画像URL：' . $profile['pictureUrl']))
-      ->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('ステータスメッセージ：' . $profile['statusMessage']))
-  );
+  // オウム返し
+  $bot->replyText($event->getReplyToken(), $event->getText());
 }
 
 // テキストを返信。引数はLINEBot、返信先、テキスト
